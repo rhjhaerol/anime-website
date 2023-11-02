007162358a01e1cd14f6acf66072924ecfeaf1dc
rhjhaerol <rhjhaerol@gmail.com>
2023-11-02 16:23:50 +0700

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,11 +1,12 @@
 import Link from "next/link"
+import InputSearch from "./InputSearch"
 
 const Navbar = () => {
     return (
-        <header className="bg-indigo-500">
-            <div className="flex flex-col md:flex-row justify-between p-4">
+        <header className="bg-color-accent">
+            <div className="flex flex-col md:flex-row justify-between md:items-center p-4">
                 <Link href="/" className=" font-bold text-white text-2xl">Neko Anime</Link>
-                <input placeholder="Cari Anime..."/>
+                <InputSearch/>
             </div>
         </header>
     )
