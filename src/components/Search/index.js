721c9e1d257a41b4c7ec61fcf72780e1c9881a71
rhjhaerol <rhjhaerol@gmail.com>
2023-11-02 13:33:41 +0700

    Create Search

---
+++ src/components/Search/index.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div>
+            <h1>Search Page</h1>
+            <p>Ini Search Page Arrow Function</p>
+        </div>
+    )
+}
+
+export default Search
