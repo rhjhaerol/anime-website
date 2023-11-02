8676bcf5109b623a5b41c2106bdb9a73a6b45329
rhjhaerol <rhjhaerol@gmail.com>
2023-11-02 16:23:54 +0700

    Create InputSearch.js

---
+++ src/components/NavBar/InputSearch.js
@@ -0,0 +1,35 @@
+"use client"
+
+import { MagnifyingGlass } from "@phosphor-icons/react/dist/ssr"
+import { useRouter } from "next/navigation"
+import { useRef } from "react"
+
+const InputSearch = () => {
+    const searchRef = useRef()
+    const router = useRouter()
+
+    const HandleSearch = (event) => {
+        if (event.key === "Enter" || event.type === "click") {
+             event.preventDefault()
+            const keyword = searchRef.current.value
+
+            router.push(`/search/${keyword}`)
+        }
+       
+    }
+    return (
+        <div className="relative">
+            <input 
+                placeholder="Cari Anime..." 
+                className="p-2 rounded w-full" 
+                ref={searchRef}
+                onKeyDown={HandleSearch}
+            />
+            <button className="absolute top-1 end-2" onClick={HandleSearch}>
+                <MagnifyingGlass size={32} />
+            </button>  
+        </div>
+    )
+}
+
+export default InputSearch
