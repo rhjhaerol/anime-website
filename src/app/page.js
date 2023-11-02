75667753deb5d289889b627d0cd6d275dae6c866
rhjhaerol <rhjhaerol@gmail.com>
2023-11-02 13:32:37 +0700

    next.js package

---
+++ src/app/page.js
@@ -0,0 +1,27 @@
+import Link from "next/link"
+import AnimeList from "@/components/AnimeList"
+
+const Home = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`)
+  const anime = await response.json()
+
+  return (
+    <div>
+      <div className="p-4 flex justify-between">
+          <h1 className="text-2xl font-bold">Top Anime</h1>
+          <Link href="/populer" className="text-xl underline hover:text-indigo-500 transition all">Lihat Semua</Link>
+      </div>
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
+        {anime.data.map(data => {
+        return (
+          <div key={data.mal_id} className="shadow-xl">
+            <AnimeList title={data.title} images={data.images.webp.image_url} id={data.mal_id}/>
+          </div>
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+
+export default Home
