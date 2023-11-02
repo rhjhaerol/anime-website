707198e7c6c8d6cc2606d5472103c3b5c85f88fd
rhjhaerol <rhjhaerol@gmail.com>
2023-11-02 13:33:07 +0700

    Create search page

---
+++ src/app/search/page.js
@@ -0,0 +1,10 @@
+const Page = () => {
+    return (
+        <div>
+            <h1>Search Page</h1>
+            <p>Ini Search Page Arrow Function</p>
+        </div>
+    )
+}
+
+export default Page
