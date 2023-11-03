105b6684b13ac55b9048d7b4d735f8a348644af1
rhjhaerol <rhjhaerol@gmail.com>
2023-11-03 12:09:46 +0700

    Create page.jsx

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import AnimeList from "@/components/AnimeList"
+import Header from "@/components/AnimeList/Header"
+
+const Page = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
+  const animeTop = await response.json()
+
+  return (
+    <>
+    <section>
+      <Header title="Top Anime" hreflink="/populer" linktitle="Lihat Semua"/>
+      <AnimeList api={animeTop}/>
+    </section>
+    </>
+  )
+}
+
+export default Page
