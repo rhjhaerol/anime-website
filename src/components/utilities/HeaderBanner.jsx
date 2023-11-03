aa30d71e37d7e16ab4dc72610fc066327355c16d
rhjhaerol <rhjhaerol@gmail.com>
2023-11-03 12:09:00 +0700

    Create HeaderBanner.jsx

---
+++ src/components/utilities/HeaderBanner.jsx
@@ -0,0 +1,9 @@
+const HeaderBanner = ({title}) => {
+    return (
+        <div className="flex justify-center items-center p-8">
+            <h3 className="text-color-primary text-2xl">{title}</h3>
+        </div>
+    )
+}
+
+export default HeaderBanner
