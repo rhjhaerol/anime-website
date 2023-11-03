2736f192d7d508c8ae3c3a7ae7df613b2d890791
rhjhaerol <rhjhaerol@gmail.com>
2023-11-03 12:09:56 +0700

    Create page.jsx

---
+++ src/app/search/[keyword]/page.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+import AnimeList from "@/components/AnimeList"
+import Header from "@/components/AnimeList/Header"
+
+const Page = async ({params}) => {
+    const keyword = params.keyword
+    const decodedKeyword = decodeURI(keyword)
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`)
+    const searchAnime = await response.json()
+
+    return (
+        <>
+        <section>
+        <Header title={`Pencarian untuk "${decodedKeyword}"...`}/>
+        <AnimeList api={searchAnime}/>
+        </section>
+        </>
+    )
+}
+
+export default Page
