ec8c8815a836a30e6b603755ee4564d2bc523148
rhjhaerol <rhjhaerol@gmail.com>
2023-11-05 11:18:14 +0700

    Update index.jsx

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <header className="bg-color-accent">
             <div className="flex flex-col md:flex-row justify-between md:items-center p-4">
-                <Link href="/" className=" font-bold text-white text-2xl">Neko Anime</Link>
+                <Link href="/" className=" font-bold text-white text-2xl">Neko Anime🐈‍⬛</Link>
                 <InputSearch/>
             </div>
         </header>
