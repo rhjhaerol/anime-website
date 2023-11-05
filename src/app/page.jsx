0425fe3be13426da71033a533b8168d731eb5ee1
rhjhaerol <rhjhaerol@gmail.com>
2023-11-05 10:59:37 +0700

    Add recommendation section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,24 @@
 import AnimeList from "@/components/AnimeList"
 import Header from "@/components/AnimeList/Header"
-import { getAnimeResponse } from "./libs/api-libs"
+import { getAnimeNested, getAnimeResponse } from "../libs/api-libs"
 
 const Page = async () => {
   
   const topAnime = await getAnimeResponse("top/anime", "limit=8")
+  let recommendedAnime = await getAnimeNested("recommendations/anime", "entry")
+  recommendedAnime = {data: recommendedAnime.slice(0,8)}
+  console.log(recommendedAnime)
 
   return (
     <>
-    <section>
-      <Header title="Top Anime" hreflink="/populer" linktitle="Lihat Semua"/>
-      <AnimeList api={topAnime}/>
-    </section>
+      <section>
+        <Header title="Top Anime" hreflink="/populer" linktitle="Lihat Semua"/>
+        <AnimeList api={topAnime}/>
+      </section>
+      <section className="pt-4">
+        <Header title="Rekomendasi"/>
+        <AnimeList api={recommendedAnime}/>
+      </section>
     </>
   )
 }
