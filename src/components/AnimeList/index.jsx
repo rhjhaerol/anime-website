da2f4d7a0d4ccd386819b0cf4b09ab7892c67e6d
rhjhaerol <rhjhaerol@gmail.com>
2023-11-05 11:09:49 +0700

    Update index.jsx

--- src/components/AnimeList/index.jsx
+++ src/components/AnimeList/index.jsx
@@ -7,9 +7,9 @@ const AnimeList = ({api}) => {
         {api.data?.map((anime, index) => {
             return (
                 <>
-                    <div className="border border-color-primary rounded">
+                    <div className="border border-color-primary rounded shadow-xl">
                         <Link href={`/anime/${anime.mal_id}`} className="cursor-pointer text-color-primary hover:text-color-accent transition-all" key={index}>
-                            <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className=" w-full max-h-64 md:object-cover"/>
+                            <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className="rounded w-full max-h-64 md:object-cover"/>
                             <h3 className="font-medium md:text-xl text-md p-4">{anime.title} - {anime.year}</h3>
                         </Link>
                     </div>
