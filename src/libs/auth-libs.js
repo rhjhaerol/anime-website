7168667839e13820ba4e775609163f0f820f71c5
rhjhaerol <rhjhaerol@gmail.com>
2023-11-06 11:48:34 +0700

    Create auth-libs.js

---
+++ src/libs/auth-libs.js
@@ -0,0 +1,8 @@
+import { getServerSession } from "next-auth"
+import { authOption } from "@/app/api/auth/[...nextauth]/route"
+
+export const authUserSession = async() => {
+    const sesion = await getServerSession(authOption)
+    
+    return sesion?.user
+}
