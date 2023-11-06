7c68e5375d0b68d57b7f6ec248e0a6637c8f58e4
rhjhaerol <rhjhaerol@gmail.com>
2023-11-06 11:48:22 +0700

    Create ActionButton.jsx

---
+++ src/components/NavBar/ActionButton.jsx
@@ -0,0 +1,20 @@
+import { authUserSession } from "@/libs/auth-libs"
+import Link from "next/link"
+
+const ActionButton = async() => {
+    const user = await authUserSession()
+    
+    const actionLabel = user ? "Sign Out" : "Sign In"
+    const actionURL = user ? "/api/auth/signout" : "/api/auth/signin"
+
+    return (
+        <div className="pt-2">
+            {
+                user ? <Link href="/users/dashboard" className="py-1 px-1">Dashboard</Link> : null
+            }
+            <Link href={actionURL} className="bg-color-dark text-color-accent py-1 px-12 inline-block rounded">{actionLabel}</Link>
+        </div>
+    )
+}
+
+export default ActionButton
