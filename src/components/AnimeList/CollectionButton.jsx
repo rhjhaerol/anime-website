fd5c240c9b8bf97c7ebb5d4ff717859075b9b2b6
rhjhaerol <rhjhaerol@gmail.com>
2023-12-04 11:41:50 +0700

    add collection button component

---
+++ src/components/AnimeList/CollectionButton.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+const CollectionButton = ({ anime_mal_id, user_email }) => {
+    const [isCreated, setIsCreated] = useState(false);
+
+    const handlerButton = async (ev) => {
+        ev.preventDefault();
+        setIsCreated(false);
+        const response = await fetch("/api/v1/collection", {
+            method: "POST",
+            body: JSON.stringify({ anime_mal_id, user_email }),
+        });
+        const collection = await response.json();
+        if (collection.isCreated) {
+            setIsCreated(true);
+        }
+    };
+
+    return (
+        <>
+            {isCreated ? (
+                <p className="text-color-primary">Berhasil ditambahkan</p>
+            ) : (
+                <button
+                    onClick={handlerButton}
+                    className="bg-color-accent px-2 py-1 my-2 rounded-md"
+                >
+                    Add to Collection
+                </button>
+            )}
+        </>
+    );
+};
+
+export default CollectionButton;
